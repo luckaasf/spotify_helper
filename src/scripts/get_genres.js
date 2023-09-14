3bd5cf149154ae8a7ca476f6f394072b18df96f2
luckaasf <125568601+luckaasf@users.noreply.github.com>
2023-09-14 22:17:17 +0100

    search box being filled if one of the buttons are clicked

--- src/scripts/get_genres.js
+++ src/scripts/get_genres.js
@@ -1,56 +1,65 @@
 const token = localStorage.getItem("access_token");
-console.log(token)
-const response = await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
-    method: 'GET',
+console.log(token);
+const response = await fetch(
+  "https://api.spotify.com/v1/recommendations/available-genre-seeds",
+  {
+    method: "GET",
     headers: {
-        'Authorization': `Bearer ${token}`,
+      Authorization: `Bearer ${token}`,
     },
-});
+  }
+);
 
 if (!response.ok) {
-    throw new Error('error fetching');
+  throw new Error("error fetching");
 }
 const data = await response.json();
-console.log(data);   
 const genre_array = data.genres;
 
 async function filteredGenres(content) {
-    const filteredGenres = genre_array.filter((genre) =>
-        genre.toLowerCase().includes(content.toLowerCase())
-    );
-    return filteredGenres;
+  const filteredGenres = genre_array.filter((genre) =>
+    genre.toLowerCase().includes(content.toLowerCase())
+  );
+  return filteredGenres;
 }
 
 const genre_search = document.getElementById("genre_id");
 const genre_results = document.getElementById("genre_results");
 
-genre_search.addEventListener('input', async () => {
-    const searchbox_content = genre_search.value.trim();
-    const filtered = await filteredGenres(searchbox_content);
-    showFilteredGenres(filtered);
+genre_search.addEventListener("input", async () => {
+  const searchbox_content = genre_search.value.trim();
+  const filtered = await filteredGenres(searchbox_content);
+  showFilteredGenres(filtered);
 });
 
-genre_search.addEventListener('blur', () => {
-    genre_results.innerHTML = "";
+genre_search.addEventListener("blur", () => {
+  genre_results.style.display = "none";
 });
 
 function showFilteredGenres(filteredGenres) {
-    genre_results.innerHTML = "";
-    
-    if (filteredGenres.length === 0) {
-        return;
-    }
+  genre_results.innerHTML = "";
 
-    let limit = 0;
-    filteredGenres.forEach(genre => {
-        limit++;
+  if (filteredGenres.length === 0) {
+    genre_results.style.display = "none";
+    return;
+  }
 
-        if (limit <= 5) {
-            const genreItem = document.createElement("div");
-            genreItem.className = "form-control";
-            genreItem.textContent = genre;
-            genre_results.appendChild(genreItem);
-        }
+  filteredGenres.slice(0, 10).forEach((genre) => {
+    const genreItem = document.createElement("button");
+    genreItem.className = "dropdown-item";
+    genreItem.type = "button";
+    genreItem.textContent = genre;
 
+    console.log(genreItem);
+
+    genreItem.addEventListener("mousedown", () => {
+      genre_search.value = genre;
     });
+
+    genre_results.appendChild(genreItem);
+  });
+
+  genre_results.style.display = "block";
 }
+
+
